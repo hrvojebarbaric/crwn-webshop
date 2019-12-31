5970605a9930b2a124daffe423e0b7d41ad9aa72
Hrvoje Barbaric <hrvoje.barbaric@hotmail.com>
2019-12-31 11:47:46 +0100

    napravljen je meni

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
@@ -10,5 +10,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
